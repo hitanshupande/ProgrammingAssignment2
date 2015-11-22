b8a179945138327c1797482f72a61ddfb19e1aad
Hitanshu Pande <hitanshu.pande@gmail.com>
2015-11-22 18:55:52 -0500

    Coursera R Programming Assignment 2
    Contains final code for cache matrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,31 @@
 ## functions do
 
 ## Write a short comment describing this function
-#TEST
+# This function creates a list that contains 4 functions: set, get, setinverse and getinverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    invrs <- NULL
+    set <- function(y) {
+        x <<- y
+        invrs <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) invrs <<- inverse
+    getinverse <- function() invrs
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve function returns the inverse of the matrix. It checks if the inverse is already calculated. In that case
+## it fetches the cached value, else it calculates and stores the inverse of the matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    invrs <- x$getinverse()
+    if(!is.null(invrs)) {
+        message("getting cached data.")
+        return(invrs)
+    }
+    data <- x$get()
+    invrs <- solve(data)
+    x$setinverse(invrs)
+    invrs
 }
+
+
